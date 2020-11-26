e92fd959c489781d1340e37bbac28957be43f123
zengli <zengli@mamahao.com>
2020-11-26 11:33:43 +0800

    新增markDown的相关语法,以及数据结构相关的脑图

---
+++ src/com/lab/algorithm/structure/SequenceList.java
@@ -0,0 +1,17 @@
+package com.lab.algorithm.structure;/**
+ * Created by zengli on 2020/11/26.
+ */
+
+/**
+ * @ClassName SequenceList
+ * @Description 顺序表
+ * @Author ZengLi
+ * @Date 2020/11/26 9:22
+ * @VerSion 1.0
+ **/
+public class SequenceList {
+    //Object类型的数组，可存储任意类型的数据
+    private Object[] data;
+    //
+    private int index;
+}
