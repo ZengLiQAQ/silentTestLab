cfbb964db2fde91da1cc2c2d6745bb618a10932f
zengli <1104908453@qq.com>
2023-08-04 23:50:30 +0800

    移除不必要的文件,调整项目接口，设置为maven项目

--- src/main/java/com/lab/silent/algorithm/solution/Solution493.java
+++ src/main/java/com/lab/silent/algorithm/solution/Solution493.java
@@ -4,7 +4,7 @@ package com.lab.silent.algorithm.solution;/**
 
 /**
  * @ClassName Solution493
- * @Description 给定一个数组 nums ，如果 i < j 且 nums[i] > 2*nums[j] 我们就将 (i, j) 称作一个重要翻转对。
+ * @Description 给定一个数组nums，如果i < j且nums[i] > 2*nums[j]我们就将(i, j)称作一个重要翻转对。
  *
  * 你需要返回给定数组中的重要翻转对的数量。
  *
