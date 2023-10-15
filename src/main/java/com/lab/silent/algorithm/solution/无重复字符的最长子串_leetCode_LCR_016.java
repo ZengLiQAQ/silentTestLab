e1bdc5d2b6ee062b1c0ed721caea51ba1eb2fc24
zengli <1104908453@qq.com>
2023-10-15 15:53:23 +0800

    练习

--- src/main/java/com/lab/silent/algorithm/solution/无重复字符的最长子串_leetCode_LCR_016.java
+++ src/main/java/com/lab/silent/algorithm/solution/无重复字符的最长子串_leetCode_LCR_016.java
@@ -2,7 +2,9 @@ package com.lab.silent.algorithm.solution;
 
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长连续子字符串 的长度。
@@ -27,11 +29,43 @@ public class 无重复字符的最长子串_leetCode_LCR_016 {
   }
 
 
+    /**
+     * LCR 016. 无重复字符的最长子串
+     *  双指针 + set(统计是否重复)
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring2(String s) {
+        int ans = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        //定义窗口边界
+        int left = 0;
+        int right = 0;
+        //终止条件为右边界滑动到字符数组末尾
+        while (right < s.length()){
+            //获取右边界当前的字符
+            char c = s.charAt(right);
+            //如果存在重复的字符,则说明右边界曾经扫描过该字符串(即上一个不重复子串已经结束,从当前开始需要重新开始结算不重复子串所以这里认为当前子串之前的字符不算重复)
+            if(map.containsKey(c) && map.get(c) >=  left){
+                left = Math.max(left+1,map.get(c)+1);
+            }
+            //记录当前子串
+            map.put(c,right);
+            //当前子串的长度
+            ans = Math.max(ans,right-left+1);
+            //移动右边界
+            right++;
+        }
+        return ans;
+    }
+
+
 
 
     public static void main(String[] args) {
-        String str = "abcdeef";
-        int s = 无重复字符的最长子串_leetCode_LCR_016.lengthOfLongestStr(str);
-        System.out.println(s);
+
+        String str2 = "tmmzuxt";
+        int s2 = 无重复字符的最长子串_leetCode_LCR_016.lengthOfLongestSubstring2(str2);
+        System.out.println(s2);
     }
 }
