2c6c4da15e416e15810c7d0177345a972464fa39
zengli <1104908453@qq.com>
2023-10-17 13:53:33 +0800

    螺旋矩阵

--- src/main/java/com/lab/silent/algorithm/solution/相交链表_leetCode160.java
+++ src/main/java/com/lab/silent/algorithm/solution/相交链表_leetCode160.java
@@ -1,5 +1,17 @@
 package com.lab.silent.algorithm.solution;
 
+import com.lab.silent.algorithm.structure.ListNode;
+
 public class 相交链表_leetCode160 {
-    
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null){
+            return null;
+        }
+        ListNode p1 = headA, p2 = headB;
+        while(p1 != p2){
+            p1 = p1 == null ? headB : p1.next;
+            p2 = p2 == null ? headA : p2.next;
+        }
+        return p1;
+    }
 }
